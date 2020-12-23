60e881964503db459054236c32fc3ee330886c6a
Sriprasath <sriprasath.cptech@gmail.com>
2020-12-23 12:52:58 +0530

    Added Navigation
    Using component interaction @Output Decorator, navigation feature was added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  feature: string = 'recipes';
+  onNavigate(event: string){
+    console.log(event);
+    this.feature = event;
+  }
 }
